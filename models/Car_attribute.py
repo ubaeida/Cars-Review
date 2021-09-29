3978f58495cd276f299c49ebaa3be4dfb6ba772b
Ubaeida <ubaeida.alkayal@gmail.com>
2021-09-29 04:26:09 +0300

    Add Cars attributes.

--- models/Car_attribute.py
+++ models/Car_attribute.py
@@ -1,11 +1,12 @@
 class CarAttribute:
 
-    def __init__(self, attribute_id, attribute_name, value_id, value):
+    def __init__(self, car_id, attribute_id, attribute_name, value_id, value):
+        self.car_id = car_id
         self.attribute_id = attribute_id
         self.attribute_name = attribute_name
         self.value_id = value_id
         self.value = value
 
     def __str__(self):
-        return f' attribute_name: {self.attribute_name}, value_id: {self.value_id}, attribute_id: {self.attribute_id},' \
-               f' value_id: {self.value_id}'
+        return f'attribute_name: {self.attribute_name}, Value: {self.value}, attribute_id: {self.attribute_id},' \
+               f' value_id: {self.value_id}, car_id: {self.car_id}'
--- servcies/attribute_service.py
+++ servcies/attribute_service.py
@@ -8,12 +8,11 @@ class AttributeService(ServiceBase):
         c = self.db.cursor()
         query = "select v.*, a.attribute_name from attribute_value v left join car_attribute a " \
                 f"on a.id = v.attribute_id where car_id= {car_id}"
-        params = (int(car_id))
         c.execute(query)
         out = c.fetchall()
-        print(out)
+        c.close()
+        attribute_list = []
         for row in out:
-            if row is not None:
-                print('searched attribute')
-                return CarAttribute(value_id=row[0], attribute_id=row[2], value=row[3], attribute_name=row[4])
-        return None
+            attribute_list.append(CarAttribute(value_id=row[0], car_id=row[1], attribute_id=row[2], value=row[3],
+                                               attribute_name=row[4]))
+        return attribute_list
